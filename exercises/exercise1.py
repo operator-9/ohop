d476f8c7043b3f56dab6611b57fda2f79bd196c1
Jon Riehl <jon.riehl@gmail.com>
2021-07-24 12:45:02 -0500

    Adds code for episode 9.

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -10,12 +10,13 @@ class TestSolution1(unittest.TestCase):
         ('1 + 2 + zab', '3 + zab'),
         ('1 + 2 + frotz + 3 + 4', '10 + frotz'),
         ('1 + 2 + frotz + "3" + "4"', '3 + frotz + "34"'),
+        ('1 + blob + 2 + blah + 3', '6 + blob + blah'),
     ]
 
     def _run_test_case(self, index):
         input_str, output_str = self.test_cases[index]
         transformer = Solution1()
-        result = transformer.visit(ast.parse(input_str))
+        result = ast.fix_missing_locations(transformer.visit(ast.parse(input_str)))
         self.assertEqual(
             ast.dump(result, indent=4),
             ast.dump(ast.parse(output_str), indent=4)
@@ -32,6 +33,9 @@ class TestSolution1(unittest.TestCase):
 
     def test_constant_fold_3(self):
         self._run_test_case(3)
+
+    def test_constant_fold_4(self):
+        self._run_test_case(4)
 
 if __name__ == '__main__':
     unittest.main()
--- exercises/solutions/solution1.py
+++ exercises/solutions/solution1.py
@@ -1,6 +1,50 @@
 import ast
 
 
-class Solution1(ast.NodeTransformer):
-    # Your code here!
-    pass
+class Episode8Solution(ast.NodeTransformer):
+    def visit_BinOp(self, node):
+        if isinstance(node.op, ast.Add):
+            left = self.visit(node.left)
+            right = self.visit(node.right)
+            if isinstance(left, ast.Constant) and isinstance(right, ast.Constant):
+                result = ast.Constant(value=left.value + right.value)
+            else:
+                result = ast.BinOp(left=left, op=ast.Add(), right=right)
+            return result
+        return self.generic_visit(node)
+
+
+class Episode9Solution(ast.NodeTransformer):
+    def visit_BinOp(self, node):
+        if isinstance(node.op, ast.Add):
+            left = self.visit(node.left)
+            right = self.visit(node.right)
+            if isinstance(right, ast.Constant):
+                if (isinstance(left, ast.BinOp) and
+                    isinstance(left.op, ast.Add)):
+                    fire = False
+                    if isinstance(left.left, ast.Constant):
+                        llvalue = left.left.value
+                        lrvalue = right.value
+                        rvalue = left.right
+                        if type(llvalue) == type(lrvalue):
+                            fire = True
+                    elif isinstance(left.right, ast.Constant):
+                        llvalue = left.right.value
+                        lrvalue = right.value
+                        rvalue = left.left
+                        if type(llvalue) == type(lrvalue):
+                            fire = True
+                    if fire:
+                        return ast.BinOp(
+                            left=ast.Constant(value=llvalue + lrvalue),
+                            op=node.op,
+                            right=rvalue
+                        )
+                elif isinstance(left, ast.Constant):
+                    return ast.Constant(value=left.value + right.value)
+            return ast.BinOp(left=left, op=node.op, right=right)
+        return self.generic_visit(node)
+
+
+Solution1 = Episode9Solution
